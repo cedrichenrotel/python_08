405f35715c28b26a9e36b0a9bc7e214655ac9f49
cedrichenrotel <cedric.henrotel@gmail.com>
2026-04-03 16:28:38 +0200

    projet fini et a envoyer dans vog

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -3,10 +3,10 @@
 #                                                      :::      ::::::::    #
 #  oracle.py                                         :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: cehenrot <cehenrot@student.42lyon.fr>     +#+  +:+       +#+         #
+#  By: cehenrot <cehenrot@student.42.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/02 14:46:16 by cehenrot        #+#    #+#               #
-#  Updated: 2026/04/03 14:06:53 by cehenrot        ###   ########.fr        #
+#  Updated: 2026/04/03 16:20:26 by cehenrot        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -36,10 +36,12 @@ def load_var() -> None:
     else:
         print("DEVELOPMENT MODE - debug enabled")
 
+    check_value = True
     for key, text in variables.items():
         value = os.getenv(key)
         if value is None:
             print(f"[WARNING] value: {key} is None")
+            check_value = False
         else:
             sensitive = ["API_KEY", "DATABASE_URL"]
             if mode == "production" and key in sensitive:
@@ -47,19 +49,27 @@ def load_var() -> None:
             else:
                 print(f"{text.capitalize()}: {value}")
 
+    print("\nEnvironment security check:")
+
+    if check_value:
+        print("[OK] No hardcoded secrets detected")
+    else:
+        print(f"[WARNING] value: {key} is None")
+    if os.path.exists('.env'):
+        print("[OK] .env file properly configured")
+    else:
+        print("[WARNING] .env file not found")
+    if mode is None:
+        print("[WARNING] no mode control")
+    else:
+        print("[OK] Production overrides available")
+
 
 def main() -> None:
     print("ORACLE STATUS: Reading the Matrix...")
 
     print("\nConfiguration loaded:")
     load_var()
-
-    print("\nEnvironment security check:")
-
-    print("[OK] No hardcoded secrets detected")
-    print("[OK] .env file properly configured")
-    print("[OK] Production overrides available")
-
     print("\nThe Oracle sees all configurations.")
 
 
